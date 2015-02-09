03e483ee9d5ef5e65a51d1c6f80949df8fa00481
Julian Kornberger <jk+github@digineo.de>
2015-02-09 02:22:58 +0100

    Reorder migrations

--- db/migrate/20150128234726_create_mx_hosts.rb
+++ db/migrate/20150128234726_create_mx_hosts.rb
@@ -2,9 +2,16 @@ class CreateMxHosts < ActiveRecord::Migration
   def change
     create_table :mx_hosts do |t|
       t.string :hostname, null: false
-      t.inet :address, null: false
+      t.inet   :address,  null: false
+
+      t.boolean :starttls
+      t.column  :tls_version,      :tls_version,      index: true
+      t.column  :tls_cipher_suite, :tls_cipher_suite, index: true
+      t.boolean :cert_valid, index: true
+      t.references :certificate, index: true
     end
 
     add_index :mx_hosts, [:address, :hostname], unique: true
+    add_foreign_key :mx_hosts, :certificates
   end
 end
