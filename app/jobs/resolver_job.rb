84710249e40eb8a7c5b674ea1032851f42e04732
Julian Kornberger <jk+github@digineo.de>
2015-03-15 22:14:39 +0100

    Remove clutter

--- app/jobs/resolver_job.rb
+++
@@ -1,8 +0,0 @@
-class ResolverJob < ActiveJob::Base
-  queue_as :default
-
-  def perform(record, method)
-    record.send method
-  end
-
-end
--- app/models/domain.rb
+++ app/models/domain.rb
@@ -4,18 +4,10 @@ class Domain < ActiveRecord::Base
 
   RESOLVER = '8.8.8.8'
 
-  after_create :enqueue_resolve
-
   scope :with_mx,       ->{ where 'ARRAY_LENGTH(mx_hosts,1) > 0' }
   scope :without_mx,    ->{ where 'ARRAY_LENGTH(mx_hosts,1) IS NULL' }
   scope :with_error,    ->{ where 'error IS NOT NULL' }
   scope :without_error, ->{ where 'error IS NULL' }
-
-  def enqueue_resolve
-    ResolverJob.perform_later(self, 'create_mx_hosts')
-  end
-
-  # SELECT COUNT(*), unnest(mx_hosts) FROM domains GROUP BY unnest(mx_hosts) ORDER BY COUNT(*) DESC;
 
   def valid_mx?
     mx_hosts.any?{|name| ICANN.fqdn?(name) }
