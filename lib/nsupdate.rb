b32511534c0c8646c1c72ac5d7b7f01609184ea0
Julian Kornberger <jk+github@digineo.de>
2015-03-23 00:48:54 +0100

    nsupdate

--- lib/nsupdate.rb
+++ lib/nsupdate.rb
@@ -1,5 +1,7 @@
 class NsUpdate
 
+  class Error < ::StandardError; end
+
   SERVER = "127.0.0.1"
   ZONE   = 'tls-scan.informatik.uni-bremen.de'
   TTL    = 3600
@@ -17,39 +19,44 @@ class NsUpdate
   end
 
   def initialize
+    @count  = 0
     @lines  = "server #{SERVER}\n"
     @lines << "zone #{ZONE}\n"
   end
 
+  # Adds a delete and update
   def update(subdomain, data)
     domain = [subdomain, ZONE].join(".")
 
     @lines << "update delete #{domain} TXT\n"
     @lines << "update add #{domain} #{TTL} TXT \"#{data}\"\n"
+    @count += 1
 
     self
   end
 
   def execute
-    @lines << "send\n"
-    self.class.nsupdate @lines
+    ms = Benchmark.ms do
+      self.class.nsupdate @lines
+    end
+    Rails.logger.info 'nsupdate to %s with %i updates (%.1fms)' % [ SERVER, @count, ms ]
   end
 
   def self.key
     Rails.root.join("data").children.find{|c| c.to_s.ends_with?(".private") } || (raise "key not found")
   end
 
+  # Executes nsupdate
   def self.nsupdate(input)
-    puts input
-
     cmd = ["nsupdate", "-k", key.to_s]
     out = IO.popen(cmd, "r+", err: [:child, :out]){ |io|
       io << input
+      io << "send\n"
       io.close_write
       io.read
     }
     if $?.to_i != 0
-      raise "#{cmd.inspect} failed: #{out}"
+      raise Error, "#{cmd.inspect} failed: #{out}"
     end
   end
 
