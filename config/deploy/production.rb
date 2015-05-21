7fb3b7fcbe5429d0cdf87912ebe3907184a36c75
Julian Kornberger <jk+github@digineo.de>
2015-05-22 00:36:18 +0200

    new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
 set :stage, :production
-set :branch, 'production'
 set :ssh_options, user: fetch(:application)
 
-server '134.102.201.91', roles: %w{web app db}
+server '91.202.41.201', roles: %w{web app db}
