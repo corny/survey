1fe95db0157dd7eec1b8cbf1b40e99a5ea1d8a80
Julian Kornberger <jk+github@digineo.de>
2015-05-21 02:40:03 +0200

    Use mozilla data to group certificates

--- app/models/root_certificates.rb
+++ app/models/root_certificates.rb
@@ -1,13 +1,30 @@
+require 'csv'
+
 class RootCertificates
 
-  ORG_MANGLE = YAML.load_file(Rails.root.join("config/ca_mangle.yml")).each do |h|
-    # Create case-insensitive Regexp instances
-    h['regex'] = Regexp.new(h['regex'],"i")
+  # Map from SHA1 fingerprint to owner
+  def self.builtin
+    # source: https://docs.google.com/spreadsheet/ccc?key=0Ah-tHXMAwqU3dGx0cGFObG9QM192NFM4UWNBMlBaekE&usp=sharing
+    @builtin ||= parse_csv(Rails.root.join("config/mozilla_CAs.csv")).map do |row|
+      [row["SHA-1 Fingerprint"].gsub(":","").downcase, row["Owner"]]
+    end.to_h
   end
 
-  OrganizationGroup = Struct.new(:name, :certs) do
+  def self.parse_csv(file)
+    rows = []
+    CSV.foreach file, headers: true do |row|
+      rows << row
+    end
+    rows
+  end
+
+  OwnerGroup = Struct.new(:name, :certs) do
     def hosts_count
       certs.map(&:count).sum
+    end
+
+    def hosts_expired_count
+      certs.map(&:expired_count).sum
     end
 
     def intermediates_count
@@ -25,9 +42,12 @@ class RootCertificates
     def to_h
       {
         name:          name,
-        hosts_count:   hosts_count,
-        intermediates: intermediates_count,
         countries:     countries,
+        intermediates: intermediates_count,
+        hosts: {
+          total:   hosts_count,
+          expired: hosts_expired_count,
+        },
         certificates: {
           total: certs.count,
           used:  used_count,
@@ -36,9 +56,12 @@ class RootCertificates
     end
   end
 
-  Entry = Struct.new(:x509, :count, :missing) do
+  Entry = Struct.new(:x509, :count, :expired_count, :missing) do
     delegate :subject, :key_size, :signature_algorithm, to: :x509
-    def organization
+    def owner
+      RootCertificates.builtin[x509.sha1] || x509_owner
+    end
+    def x509_owner
       (subject["O"] || subject["CN"] || []).first
     end
     def intermediates
@@ -54,7 +77,7 @@ class RootCertificates
 
   def initialize
     @unassigned = RawCertificate
-    .select("raw_certificates.id, raw_certificates.raw, count(*) AS count")
+    .select("raw_certificates.id, raw_certificates.raw, count(*) AS count, count(CASE WHEN cert_expired THEN 1 ELSE null END) expired_count")
     .joins("INNER JOIN mx_hosts ON mx_hosts.chain_root_id=raw_certificates.id")
     .group(:id)
     .map do |cert|
@@ -63,11 +86,12 @@ class RootCertificates
 
     @entries = Dir["/usr/share/ca-certificates/mozilla/*.crt"].map do |path|
       x509 = OpenSSL::X509::Certificate.new File.read(path)
-      Entry.new x509, @unassigned.delete(x509.sha1(binary: true)).try(:count) || 0
+      row  = @unassigned.delete(x509.sha1(binary: true))
+      Entry.new x509, *( row ? [row.count, row.expired_count] : [0,0] )
     end
 
     @unassigned.values.each do |cert|
-      Entry.new cert.x509, cert.count, true
+      Entry.new cert.x509, cert.count, cert.expired_count, true
     end
 
     @entries.sort_by!{|e| -e.count }
@@ -82,19 +106,11 @@ class RootCertificates
     @entries.group_by{|e| [e.signature_algorithm,e.key_size] }.sort_by{|key,_| key}
   end
 
-  def mangle_organization(name)
-    name.force_encoding("utf-8")
-    ORG_MANGLE.each do |h|
-      return h['name'] if name =~ h['regex']
-    end
-    name
-  end
-
-  # grouped by organization and sorted descending by number of hosts
-  def by_organization
+  # grouped by owner and sorted descending by number of hosts
+  def by_owner
     @entries
-    .group_by{|e| mangle_organization(e.organization) }
-    .map{|name,certs| OrganizationGroup.new name, certs }
+    .group_by(&:owner)
+    .map{|name,certs| OwnerGroup.new name, certs }
     .sort_by{|grp| [-grp.hosts_count, grp.name.downcase] }
   end
 
